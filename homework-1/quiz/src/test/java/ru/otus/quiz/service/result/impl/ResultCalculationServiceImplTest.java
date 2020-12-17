6b98673dbfd28209c6f3de6341474f349863bd6d
bolshalex <bolshalex@gmail.com>
2020-12-17 17:55:27 +0300

    added test

--- homework-1/quiz/src/test/java/ru/otus/quiz/service/result/impl/ResultCalculationServiceImplTest.java
+++ homework-1/quiz/src/test/java/ru/otus/quiz/service/result/impl/ResultCalculationServiceImplTest.java
@@ -1,7 +1,10 @@
 package ru.otus.quiz.service.result.impl;
 
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import ru.otus.quiz.model.Player;
 import ru.otus.quiz.model.PlayerAnswer;
 import ru.otus.quiz.model.Question;
@@ -9,13 +12,20 @@ import ru.otus.quiz.model.QuizResult;
 import ru.otus.quiz.service.result.ResultCalculationService;
 
 import java.util.*;
+import java.util.stream.Stream;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ResultCalculationServiceImplTest {
-    @Test
-    void calcResult() {
-        ResultCalculationService resultCalculationService = new ResultCalculationServiceImpl(1);
+
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("provideTestData")
+    void testQuizResult(String testName,
+                        int requiredCorrectedAnswers,
+                        Map<Player, QuizResult> expectedPlayerQuizResult) {
+
+        ResultCalculationService resultCalculationService = new ResultCalculationServiceImpl(requiredCorrectedAnswers);
         Map<Player, QuizResult> playerQuizResult = resultCalculationService.calcResult(getPlayerAnswers());
-        Assertions.assertThat(playerQuizResult).isEqualTo(getExpectedPlayerResult());
+        Assertions.assertThat(playerQuizResult).isEqualTo(expectedPlayerQuizResult);
     }
 
     private Map<Player, List<PlayerAnswer>> getPlayerAnswers() {
@@ -23,6 +33,13 @@ class ResultCalculationServiceImplTest {
         Map<Player, List<PlayerAnswer>> playerAnswers = new HashMap<>();
         playerAnswers.put(player, getAnswers());
         return playerAnswers;
+    }
+
+    private Stream<Arguments> provideTestData() {
+        return Stream.of(
+                Arguments.of("Test successful quiz", 1, getSuccessfulPlayerResult()),
+                Arguments.of("Test fail quiz", 2, getFailPlayerResult())
+        );
     }
 
     private List<PlayerAnswer> getAnswers() {
@@ -40,9 +57,17 @@ class ResultCalculationServiceImplTest {
         return answers;
     }
 
-    private Map<Player, QuizResult> getExpectedPlayerResult() {
+    private Map<Player, QuizResult> getSuccessfulPlayerResult() {
         Player player = new Player("Player1");
         QuizResult quizResult = new QuizResult(1, true);
+        Map<Player, QuizResult> playerQuizResult = new HashMap<>();
+        playerQuizResult.put(player, quizResult);
+        return playerQuizResult;
+    }
+
+    private Map<Player, QuizResult> getFailPlayerResult() {
+        Player player = new Player("Player1");
+        QuizResult quizResult = new QuizResult(1, false);
         Map<Player, QuizResult> playerQuizResult = new HashMap<>();
         playerQuizResult.put(player, quizResult);
         return playerQuizResult;
