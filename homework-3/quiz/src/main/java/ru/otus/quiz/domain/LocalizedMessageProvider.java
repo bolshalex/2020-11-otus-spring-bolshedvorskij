d0e618397f258a22abb64673c1ced9393682668b
bolshalex <bolshalex@gmail.com>
2020-12-29 23:11:54 +0300

    added localized message provider

---
+++ homework-3/quiz/src/main/java/ru/otus/quiz/domain/LocalizedMessageProvider.java
@@ -0,0 +1,29 @@
+package ru.otus.quiz.domain;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Component;
+import ru.otus.quiz.config.QuizProperty;
+
+import java.util.Locale;
+
+@Component
+public class LocalizedMessageProvider {
+    private Locale locale;
+    private MessageSource messageSource;
+
+    @Autowired
+    public LocalizedMessageProvider(MessageSource messageSource, QuizProperty quizProperty) {
+        this.messageSource = messageSource;
+        locale = quizProperty.getLocale();
+    }
+
+    public String getMessage(String messageCode) {
+        return messageSource.getMessage(messageCode, null, locale);
+    }
+
+    public String getMessage(String messageCode, String... args) {
+        return messageSource.getMessage(messageCode, args, locale);
+    }
+
+}
